db9b1ef702e4ed70b45de9c27bded38f41014b5d
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-02-13 13:32:17 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,11 @@
 # Your code goes here!
 class Anagram
-  attr_accessor :email 
-  def initialize(word)
+  
+  attr_accessor :word
+  
+  def initialize(words)
+    @word = words 
+  end
 
 
   def match
